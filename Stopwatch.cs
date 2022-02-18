3d9c20ef9685b2be21a15d3ca3990d55fe1e91f0
Kelly Barbosa <kbadsilva@gmail.com>
2022-02-18 19:35:17 -0300

    versão 0.0.1

---
+++ Stopwatch.cs
@@ -0,0 +1,91 @@
+﻿namespace Stopwatch
+{
+    class Stopwatch
+    {
+        static void Main(String[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("S = Segundo");
+            Console.WriteLine("M = Minuto");
+            Console.WriteLine("H = Hora");
+            Console.WriteLine("Deseja cronometrar: ");
+            string tpTime = Console.ReadLine();
+
+            Console.WriteLine("Quanto tempo deseja contar?");
+                int count = int.Parse(Console.ReadLine());
+
+            switch (tpTime)
+            {
+                case "S":
+                    CalcSeconds(count);
+                    break;
+                case "M":
+                    CalcMinutes(count);
+                    break;
+                case "H":
+                    CalcHours(count);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        static void CalcSeconds(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado !!");
+            Thread.Sleep(2500);
+
+        }
+        static void CalcMinutes(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(60000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado !!");
+            Thread.Sleep(2500);
+
+        }
+        static void CalcHours(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(3600 * 1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado !!");
+            Thread.Sleep(2500);
+
+        }
+    }
+}
